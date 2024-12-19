e4fc4532f8459b72f80ea1868d251a666f90d720
Nathan Hsiao <Nathanhsiao123@gmail.com>
2024-12-19 13:30:40 -0600

    Add space again

--- Arrays/remove_element.py
+++ Arrays/remove_element.py
@@ -1,10 +1,10 @@
-#Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
-#The relative order of the elements should be kept the same. 
-#Then return the number of unique elements in nums.
+# Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+# The relative order of the elements should be kept the same. 
+# Then return the number of unique elements in nums.
 
 
-#Input: nums = [0,0,1,1,1,2,2,3,3,4]
-#Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+# Input: nums = [0,0,1,1,1,2,2,3,3,4]
+# Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 
 class Solution(object):
     def removeElement(self, nums, val):
--- Arrays/search_insert_pos.py
+++ Arrays/search_insert_pos.py
@@ -1,5 +1,5 @@
 # Given a sorted array of distinct integers and a target value, return the index if the target is found. 
-#If not, return the index where it would be if it were inserted in order.
+# If not, return the index where it would be if it were inserted in order.
 
 # Input: nums = [1,3,5,6], target = 5
 # Output: 2
