6982672637bb515c95c836317cf14fcbcef99ac1
Nathan Hsiao <Nathanhsiao123@gmail.com>
2024-12-19 13:28:11 -0600

    problem statement for

--- Arrays/search_insert_pos.py
+++ Arrays/search_insert_pos.py
@@ -1,3 +1,9 @@
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+
+
 class Solution(object):
     def searchInsert(self, nums, target):
         """
