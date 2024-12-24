d49e9d78a308bb0f53f698d4f5bf2121ad27cd2f
nathan hsiao <nathanhsiao123@gmail.com>
2024-12-24 04:18:34 -0500

    add code to valid parenthese

--- String/valid_parentheses.py
+++ String/valid_parentheses.py
@@ -1,4 +1,39 @@
-       
+# Input: s = "()[]{}"
+# Output: true
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        open_char = ['(',  '{', '[']
+        close_char = [')',  '}', ']']
+
+        for letter in s:
+            if letter in close_char and not stack:
+                return False
+            if letter in open_char:
+                stack.append(letter)
+            elif letter == '}':
+                top = stack.pop()
+                if top != '{':
+                    return False
+            elif letter == ']':
+                top = stack.pop()
+                if top != '[':
+                    return False
+            elif letter == ')':
+                top = stack.pop()
+                if top != '(':
+                    return False
+        
+        if stack:
+            return False
+        else:
+            return True
+                   
 
 
 
