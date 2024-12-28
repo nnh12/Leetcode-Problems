ea8139e979661e733e88169e43f9c5d9e3274d6d
nathan hsiao <nathanhsiao123@gmail.com>
2024-12-28 10:19:04 -0500

    remove visit list and mark directly onto the grid

--- Graphs/num_islands.py
+++ Graphs/num_islands.py
@@ -11,7 +11,6 @@ Output: 1
 
 class Solution(object):
     def __init__(self):
-        self.visit = []
         self.grid = []
         self.m = 0
         self.n = 0
@@ -22,7 +21,7 @@ class Solution(object):
         list = []
 
         if row+1 >= 0 and row+1 < self.m and self.grid[row+1][column] == '1':
-                list.append([row+1, column])
+            list.append([row+1, column])
         if row-1 >= 0 and row-1 < self.m and self.grid[row-1][column] == '1':
             list.append([row-1, column])
         if column+1 >= 0 and column+1 < self.n and self.grid[row][column+1] == '1':
@@ -38,14 +37,12 @@ class Solution(object):
         while queue:
             top = queue.popleft()
             neighbor = self.get_neighbor(top)
-            if top not in self.visit:
-                self.visit.append(top)
-            #print(top)
+            self.grid[top[0]][top[1]] = '0'
 
             if neighbor:
                 for n in neighbor:
-                    if n not in self.visit:
-                        self.visit.append(n)
+                    if self.grid[n[0]][n[1]] == '1':
+                        self.grid[n[0]][n[1]] = '0'
                         queue.append(n)
 
     def numIslands(self, grid):
@@ -56,14 +53,11 @@ class Solution(object):
         self.m = len(grid)
         self.n = len(grid[0])
         self.grid = grid
-
         islands = 0
 
         for row in range(self.m):
             for col in range(self.n):
-                if [row, col] not in self.visit and self.grid[row][col] == '1':
-                    #print([row, col])
-                    #print(self.visit)
+                if self.grid[row][col] == '1':
                     islands += 1
                     self.bfs(row, col)
         
