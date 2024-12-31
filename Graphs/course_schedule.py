c9ac513dbf1232b2c8158b12a252555c6406c4eb
nathan hsiao <nathanhsiao123@gmail.com>
2024-12-31 03:41:20 -0500

    Add problem statement

--- Graphs/course_schedule.py
+++ Graphs/course_schedule.py
@@ -1,3 +1,11 @@
+'''
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and
+to take course 0 you should also have finished course 1. So it is impossible.
+'''
+
 class Solution(object):
     def __init__(self):
         self.matrix = {}
