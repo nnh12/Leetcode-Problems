dcc4279c430bb6c77e0e844756982e83de34cd83
nathan hsiao <nathanhsiao123@gmail.com>
2025-01-05 23:36:18 -0500

    process popping parent directory correctly

--- String/simplify_path.py
+++ String/simplify_path.py
@@ -4,47 +4,35 @@ class Solution(object):
         :type path: str
         :rtype: str
         """
-        i = 1
-        list = []
-        stack = []
-        string = ""
-
+        i=1
         while (i < len(path)):
-            if (path[i] == '/' and path[i-1] == '/'):
+            if path[i-1] == '/' and path[i] == '/':
                 path = path[:i] + path[i+1:]
             else:
                 i += 1
         
+        i=1
+        start = 0
+        list = []
         print(path)
-        i = 0
-        while (i < len(path)):
-            if path[i] == '/':
-                list.append(path[i])
-                i += 1
-            else:
-                for index in range(i, len(path)):
-                    if path[index] == '/':
-                        break
-                list.append(path[i:index])
-                i = index + 1
 
-        print(list)
+        while i < len(path):
+            start = i
+            while (i < len(path) and path[i] != '/'):
+                i += 1
+                
+            list.append(path[start:i])
+            i+=1
+        
+        stack = []
         index = 0
-        while (index < len(list)):
-            if list[index] == ".." and index != 1:
+        while(index < len(list)):
+            if list[index] == ".." and index + 1 < len(list):
                 stack.pop()
-                stack.append(list[index + 1])
+                stack.append(list[index+1])
                 index += 2
             else:
                 stack.append(list[index])
                 index += 1
-        
-        print(stack)
 
-        for index in range(len(stack)):
-            string += stack[index]
-            if index >= 1:
-                string += '/'
-            
-        return string[:len(string)-1]
-        
+        print(stack)
