a41995de4651ea3579bc48369a7ff0c5f937c5de
nathan hsiao <nathanhsiao123@gmail.com>
2025-01-22 19:24:09 -0500

    my attempt at word search

--- String/word_search.py
+++ String/word_search.py
@@ -1,29 +1,33 @@
 class Solution(object):
+
     def __init__(self):
-        self.board = []
+        self.board = None
+        self.word = ""
         self.m = 0
         self.n = 0
-        self.word = ""
 
     def dfs(self, row, col, index):
-        if index == len(self.word):
+        if index == len(self.word) :
             return True
         
         temp = self.board[row][col]
         self.board[row][col] = "#"
-        
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        
-        for dr, dc in directions:
-            new_row, new_col = row + dr, col + dc
-            
-            if 0 <= new_row < self.m and 0 <= new_col < self.n and self.board[new_row][new_col] == self.word[index]:
-                if self.dfs(new_row, new_col, index + 1):  # Recursive DFS call
-                    return True
-        
-        self.board[row][col] = temp
-        return False
 
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+
+        for n in directions:
+            r, c = n
+            new_r = row + r
+            new_c = col + c
+
+            print(self.board[row][col])
+            if ((new_r >= 0) and (new_c >= 0) and (new_r < self.m) and (new_c < self.n) and self.board[new_r][new_c] == self.word[index]):
+                if (self.dfs(new_r, new_c, index + 1)):
+                    return True
+
+        self.board[row][col] = temp
+        return False    
+        
     def exist(self, board, word):
         """
         :type board: List[List[str]]
@@ -34,12 +38,14 @@ class Solution(object):
         self.m = len(board)
         self.n = len(board[0])
         self.word = word
-        
-        for row in range(self.m):
-            for col in range(self.n):
-                # Start DFS if the first character matches
-                if self.board[row][col] == self.word[0]:
-                    if self.dfs(row, col, 1):  # Start the search from the next character
-                        return True
-        
-        return False
+
+        if not board:
+            return False
+        else:
+            for i in range((self.m)):
+                for j in range((self.n)):
+                    if self.board[i][j] == self.word[0]:
+                        if self.dfs(i, j, 1):
+                            return True
+            
+            return False
