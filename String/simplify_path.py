522790df0eb62b48a68e03769d96cf338d90476b
nathan hsiao <nathanhsiao123@gmail.com>
2025-01-24 00:09:11 -0500

    successful pass for all edge cases

--- String/simplify_path.py
+++ String/simplify_path.py
@@ -24,36 +24,20 @@ class Solution(object):
             i+=1
         
         stack = []
-        index = 0
-
-        while(index < len(list)):
-            if list[index] == ".." and index + 1 < len(list):
+        for file in list:
+            if file != ".." and file != ".":
+                stack.append(file)
+            elif file== ".." and stack:
                 stack.pop()
-                stack.append(list[index+1])
-                index += 2
-            else:
-                stack.append(list[index])
-                index += 1
+            print(stack)
+        
+        str = "/"
 
-        word_stack = []
-        print(stack)
+        for file in stack:
+            str += file
+            str += "/"
+        
+        if str == "/":
+            return str
 
-        for i in stack:
-            if i == ".." and word_stack:
-                word_stack.pop()
-            elif i != ".":
-                word_stack.append(i)
-       
-        print(word_stack)
-        stack = word_stack
-
-        if len(stack) == 1 and stack[0] == "..":
-            return "/"
-
-        path = "/"
-        for i in stack:
-            if i != '.':
-                path += i
-                path += "/"
-
-        return path[:len(path) -1]
+        return str[:len(str) -1]
