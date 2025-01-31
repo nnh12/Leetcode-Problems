7d0c8b71d487a7562bc839ce1439687f7da2dc9b
nathan hsiao <nathanhsiao123@gmail.com>
2025-01-31 14:48:56 -0600

    try using sliding window

--- String/longest_repeating_char.py
+++ String/longest_repeating_char.py
@@ -1,28 +1,53 @@
 class Solution(object):
+    def __init__(self):
+        self.dict = {}
+        self.max = 0
+    
+    def add_char(self, char):
+        if char in self.dict:
+            self.dict[char] += 1
+        else:
+            self.dict[char] = 1
+        
+        self.max = max(self.max, self.dict[char])
+    
+    def remove_char(self, char):
+        if char in self.dict:
+            self.dict[char] -= 1
+        
+        max = 0
+        for keys in self.dict:
+            if self.dict[keys] > max:
+                max = self.dict[keys]
+        
+        self.max = max
+
+
     def characterReplacement(self, s, k):
         """
         :type s: str
         :type k: int
         :rtype: int
         """
-
-        max  = 0 
-
-        for i in range(len(s)):
-            start = s[i]
-            iterate = i
-            cur_count = 0
-            replac_count = k
-
-            while (iterate < len(s) and (start == s[iterate] or replac_count > 0) ):
-                if (start != s[iterate]):
-                    replac_count -= 1
-                
-                iterate += 1
-                cur_count += 1
-            
-            if cur_count > max:
-                max = cur_count
+        win_size = 0
+        right, left = 0, 0
         
-        return max
+        while (right < len(s)):
 
+            num_changes = (right - left + 1) - self.max
+
+            if (right- left + 1 > win_size):
+                print(self.max)
+                print(right, left)
+                print(" ")
+                win_size = right-left + 1
+
+            if (num_changes > k):
+                self.remove_char(s[left])
+                left += 1
+            else:
+                self.add_char(s[right])
+                right += 1
+
+        print(self.max)
+        return win_size
