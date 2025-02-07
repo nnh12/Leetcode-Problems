23d2960af4044bd60311d850281e62d694137160
nathan hsiao <nathanhsiao123@gmail.com>
2025-02-06 22:42:02 -0500

    correct dfs traversal from ocean and use deep copy for visit reference

--- Graphs/pacific_atlantic_water_flow.py
+++ Graphs/pacific_atlantic_water_flow.py
@@ -1,60 +1,62 @@
+import copy 
+
 class Solution(object):
 
     def __init__(self):
         self.m = 0
         self.n = 0
         self.heights = [[]]
-        self.copy_heights = [[]]
+        self.visit = []
 
     def neighbors(self, row, col):
         list = []
-        print('compare point to', row, col)
 
-        if row-1 >=0 and (self.heights[row-1][col] <= self.heights[row][col]):
+        if (row - 1 >= 0 and (self.heights[row-1][col] <= self.heights[row][col]  )):
+            print(self.heights[row-1][col], 'is less than', self.heights[row][col])
             list.append([row-1, col])
-        if col -1 >= 0 and (self.heights[row][col-1] <= self.heights[row][col]):
+        if (col - 1 >= 0 and (self.heights[row][col - 1] <= self.heights[row][col]) ):
+            print(self.heights[row][col - 1], 'is less than', self.heights[row][col])
             list.append([row, col-1])
-        if row+ 1 < self.m and (self.heights[row+1][col] <= self.heights[row][col]):
-            list.append([row + 1, col])
-        if col + 1 < self.n and (self.heights[row][col+1] <= self.heights[row][col]):
-            print('here')
-            list.append([row, col + 1])
+        if (row + 1 < self.m and (self.heights[row+1][col] <= self.heights[row][col]) ):
+            print(self.heights[row+1][col], 'is less than', self.heights[row][col])
+            list.append([row+1, col])
+        if (col + 1 < self.n and (self.heights[row][col+1]) <= self.heights[row][col] ):
+            print(self.heights[row][col + 1], 'is less than', self.heights[row][col])
+            list.append([row, col+1])
 
-        print(row, col, list)
         return list
 
-    def dfs(self, row_init, col_init ):
+    def dfs(self, row, col):
         stack = []
-        stack.append([row_init, col_init])
+        stack.append([row, col])
+        visit = copy.deepcopy(self.heights)
+        visit[row][col] = -1
         oceans = [0, 0]
-        copy_heights = self.copy_heights
         
         while stack:
-            top = stack.pop()
-            row, col = top  
-            print([row, col])
+            row_cur, col_cur = stack.pop()
+            neighbors = self.neighbors(row_cur, col_cur)
+            print([row_cur, col_cur])
 
-            neighbors = self.neighbors(row, col)
-
-            if (row == 0) or (col == 0):
-                print(row, col)
+            if (row_cur == 0) or (col_cur == 0):
                 oceans[0] = 1
             
-            if (row == self.m -1) or (col == self.n-1):
-                print(row, col)
+            if (row_cur == (self.m - 1) or col_cur == (self.n - 1)):
                 oceans[1] = 1
             
             if oceans == [1, 1]:
-                return [1, 1]
+                print('yes')
+                return True
 
-            for n in neighbors:
-                row_n, col_n = n
-                if self.copy_heights[row_n][col_n] != "*":
-                    self.copy_heights[row_n][col_n] = "*"
-                    stack.append([row_n, col_n])
+            if neighbors:
+                for n in neighbors:
+                    if visit[n[0]][n[1]] != -1 :
+                        print(n, 'not in ', self.visit)
+                        visit[n[0]][n[1]] = -1
+                        stack.append(n)
 
-        self.copy_heights = copy_heights
-        return oceans
+        return False
+
 
     def pacificAtlantic(self, heights):
         """
@@ -64,19 +66,12 @@ class Solution(object):
         self.heights = heights
         self.m = len(heights)
         self.n = len(heights[0])
-        self.copy_heights = heights
         list = []
-
-        if heights == [[1]]:
-            return []
         
-        print(self.dfs(2, 2))
-
-#        for i in range(self.m):
-#            for j in range(self.n):
-#                if self.dfs(i, j) == [1, 1]:
-#                    list.append([i, j])
-
+        for i in range(self.m):
+            for j in range(self.n):
+                if self.dfs(i, j):
+                    list.append([i, j])
         print(list)
         return list
 
