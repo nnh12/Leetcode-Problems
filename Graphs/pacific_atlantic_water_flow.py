d83121ff8fb938652e1211eb89ffa1911714d169
nathan hsiao <nathanhsiao123@gmail.com>
2025-02-07 00:13:23 -0500

    iterate through the start of each edge of ocean and return joined paths

--- Graphs/pacific_atlantic_water_flow.py
+++ Graphs/pacific_atlantic_water_flow.py
@@ -1,5 +1,3 @@
-import copy 
-
 class Solution(object):
 
     def __init__(self):
@@ -7,18 +5,19 @@ class Solution(object):
         self.n = 0
         self.heights = [[]]
         self.visit = []
-        self.dp = []
+        self.visit_2 = []
+        self.islands = []
 
     def neighbors(self, row, col):
         add_set = set()
 
-        if (row - 1 >= 0 and (self.heights[row-1][col] <= self.heights[row][col]  )):
+        if (row - 1 >= 0 and (self.heights[row-1][col] >= self.heights[row][col]  )):
             add_set.add((row-1, col))
-        if (col - 1 >= 0 and (self.heights[row][col - 1] <= self.heights[row][col]) ):
+        if (col - 1 >= 0 and (self.heights[row][col - 1] >= self.heights[row][col]) ):
             add_set.add((row, col-1))
-        if (row + 1 < self.m and (self.heights[row+1][col] <= self.heights[row][col]) ):
+        if (row + 1 < self.m and (self.heights[row+1][col] >= self.heights[row][col]) ):
             add_set.add((row+1, col))
-        if (col + 1 < self.n and (self.heights[row][col+1]) <= self.heights[row][col] ):
+        if (col + 1 < self.n and (self.heights[row][col+1]) >= self.heights[row][col] ):
             add_set.add((row, col+1))
 
         return add_set
@@ -26,37 +25,36 @@ class Solution(object):
     def dfs(self, row, col):
         stack = []
         stack.append([row, col])
+        self.visit[row][col] = -1
 
-        visit = []
-        for _ in range(self.m):
-            r = [0] * self.n
-            visit.append(r)
-        
-        visit[row][col] = -1
-        oceans = [0, 0]
-        
         while stack:
             row_cur, col_cur = stack.pop()
             neighbors = self.neighbors(row_cur, col_cur)
-            print([row_cur, col_cur])
-
-            if (row_cur == 0) or (col_cur == 0):
-                oceans[0] = 1
-            
-            if (row_cur == (self.m - 1) or col_cur == (self.n - 1)):
-                oceans[1] = 1
-            
-            if oceans == [1, 1] or self.dp[row_cur][col_cur] == -1:
-                return True
 
             if neighbors:
                 for n in neighbors:
-                    if visit[n[0]][n[1]] != -1 :
-                        visit[n[0]][n[1]] = -1
+                    if self.visit[n[0]][n[1]] == 0 :
+                        self.visit[n[0]][n[1]] = -1
                         stack.append(n)
+    
+    def dfs_atlantic(self, row, col):
+        stack = []
+        stack.append([row, col])
+        self.visit_2[row][col] = -1
 
-        return False
+        while stack:
+            row_cur, col_cur = stack.pop()
+            neighbors = self.neighbors(row_cur, col_cur)
 
+            if self.visit[row_cur][col_cur] == -1:
+                self.visit[row_cur][col_cur] = 0 
+                self.islands.append([row_cur, col_cur])
+                
+            if neighbors:
+                for n in neighbors:
+                    if self.visit_2[n[0]][n[1]] == 0 :
+                        self.visit_2[n[0]][n[1]] = -1
+                        stack.append(n)
 
     def pacificAtlantic(self, heights):
         """
@@ -66,15 +64,24 @@ class Solution(object):
         self.heights = heights
         self.m = len(heights)
         self.n = len(heights[0])
-        self.dp = copy.deepcopy(heights)
-        list = []
         
-        for i in range(self.m):
-            for j in range(self.n-1, -1, -1):
-                if self.dfs(i, j):
-                    list.append([i, j])
-                    self.dp[i][j] = - 1
-                   
-        return list
+        islands = []
+        for _ in range(self.m):
+            r = [0] * self.n
+            r_2 = [0] * self.n
+            self.visit.append(r)
+            self.visit_2.append(r_2)
+    
+        for col in range(self.n):
+            self.dfs(0, col)
+        
+        for row in range(self.m):
+            self.dfs(row, 0)
+        
+        for row in range(self.m):
+            self.dfs_atlantic(row, self.n - 1)
+ 
+        for col in range(self.n):
+            self.dfs_atlantic(self.m - 1, col)
 
-        
+        return self.islands
