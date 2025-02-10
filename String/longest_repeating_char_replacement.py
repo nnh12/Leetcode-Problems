00439b1cbd8f484ee91fdcc6acd898a0aaa54ea5
nathan hsiao <nathanhsiao123@gmail.com>
2025-02-10 00:00:14 -0500

    my version of longest character replacement

--- String/longest_repeating_char_replacement.py
+++ String/longest_repeating_char_replacement.py
@@ -1,60 +1,50 @@
 class Solution(object):
     def __init__(self):
-        self.max_count = 0
-        self.char_dict = {}
-        self.max_char = ""
+        self.dict = {}
+        self.max = 0
+        self.char = ""
 
+    
     def add_char(self, char):
-
-        if char in self.char_dict:
-            print('add ', char)
-            self.char_dict[char] += 1
+        if char in self.dict:
+            self.dict[char] += 1
         else:
-            self.char_dict[char] = 1
-
-        if self.char_dict[char] > self.max_count:
-            self.max_count = self.char_dict[char]
-            self.max_char = char
+            self.dict[char] = 1
         
+        if self.dict[char] > self.max:
+            self.max = self.dict[char]
+            self.char = char
+
         return True
+    
+    def remove_char(self,char):
+        if char in self.dict:
+            self.dict[char] -= 1
+            self.max -= 1
+            max = self.max
 
-    def remove_char(self, char):
-        if char in self.char_dict:
-            if self.char_dict[char] != 0:
-                print('remove ', char)
-                self.char_dict[char] -= 1
-            else:
-                self.char_dict[char] = 0
-        
-        if self.max_char == char:
-            self.max_count -= 1
-        
- 
+            for key in self.dict:
+                if self.dict[key] > max:
+                    self.char = key
+                    self.max = self.dict[key]
+    
     def characterReplacement(self, s, k):
         """
         :type s: str
         :type k: int
         :rtype: int
         """
-        max = 0
+        max = 0 
         right = 0
-        ptr = 1
-        left = 0
+        
+        for left in range(len(s)):
 
-
-        for i in range(len(s)):
-            left = i
-
-            while (right < len(s) and self.add_char(s[right]) and (right - left + 1 - self.max_count <= k)):
-                if (right - left + 1 > max):
-                    max = right - left + 1
-                print(s[right], right, left, self.char_dict, self.max_count, right - left + 1 - self.max_count)
-
+            while (right < len(s) and self.add_char(s[right]) and (right - left + 1 - self.max <= k )  ):
+                print(right, left, self.dict, self.max, self.char)
                 right += 1
             
-            print('here')
+            print(right, left, self.dict, self.max, right - left + 1 - self.max, 'end')
             self.remove_char(s[left])
-            if (right < len(s)):
-                self.remove_char(s[right])
-                   
+               
         return max
+
