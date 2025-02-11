ade9b255c38a6b5aacad7686e5d2819c9e69172a
nathan hsiao <nathanhsiao123@gmail.com>
2025-02-10 23:01:46 -0500

    convoluted approach of tracking parent nodes (but passes all edge cases)

--- Trees/validate_binary_tree.py
+++ Trees/validate_binary_tree.py
@@ -6,64 +6,87 @@
 #         self.right = right
 
 class ParentNode(object):
-    def __init__(self, val=0, parent=None):
+    def __init__(self, val=0, right_dir=False):
         self.val = val
         self.left = None
         self.right = None
         self.parent = []
-    
-    def add_node_left(self, node):
-        self.left = node
-    
-    def add_node_right(self, node):
-        self.right = node
+        self.right_dir = None
+        self.dir = []
 
     def add_parent(self, val):
         self.parent.append(val)
     
+    def add_dir(self, val):
+        self.dir.append(val)
+
     def get_parent(self):
         return self.parent
+    
+    def get_dir(self):
+        return self.dir
+    
+    def add_list(self, node, dor):
+        self.parent.extend(node)
+        self.dir.extend(dor)
+    
+    def print_1(self, n):
+        print(n, [node for node in self.parent], [node for node in self.dir])
     
 
 class Solution(object):
 
     def bfs(self, node):
         queue = deque()
-        parent_tree = ParentNode(node.val)
-        queue.append([node, parent_tree])
+        tree = ParentNode(node.val)
+        queue.append([node, tree])
 
         while queue:
-            top, parent = queue.popleft()
+            top, tree = queue.popleft()
+            
+            if top.right:
+                tree.right = ParentNode(top.right.val)
+                tree.right.add_parent(tree.val)
+                tree.right.add_dir(True)
+
+                if tree.get_parent():
+                    tree.right.add_list(tree.get_parent(), tree.get_dir())
+            
+                cur_val = top.right.val
+                dir_list = tree.right.get_dir()
+                parent_val = tree.right.get_parent()
+    
+                for index in range(len(parent_val)):
+                    if (dir_list[index] and cur_val <= parent_val[index]) or (not dir_list[index] and cur_val >= parent_val[index]):
+                        return False
+                
+                queue.append([top.right, tree.right])
             
             if top.left:
-                parent.left = ParentNode(top.left.val)
-                parent.left.add_parent(top.val)
-                if parent.get_parent():
-                    parent.left.add_parent(parent.get_parent())
+                tree.left = ParentNode(top.left.val, False)
+                tree.left.add_parent(tree.val)
+                tree.left.add_dir(False)
 
-                print(top.left.val, parent.left.get_parent())                
+                if tree.get_parent():
+                    tree.left.add_list(tree.get_parent(), tree.get_dir())
                 
-                queue.append([top.left, parent.left])
-                
-            if top.right:
-                parent.right = ParentNode(top.right.val)
-                parent.right.add_parent(top.val)
-                if parent.get_parent():
-                    parent.right.add_parent(parent.get_parent())
+                cur_val = top.left.val
+                dir_list = tree.left.get_dir()
+                parent_val = tree.left.get_parent()
 
-                print(top.right.val, parent.right.get_parent())
+                for index in range(len(parent_val)):
+                    if (dir_list[index] and cur_val <= parent_val[index]) or (not dir_list[index] and cur_val >= parent_val[index]):
+                        return False
 
-                queue.append([top.right, parent.right])
-        
-        return parent_tree
+                queue.append([top.left, tree.left])
+
+        return True
 
     def isValidBST(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: bool
         """
-        tree = self.bfs(root)
-        print(' ')
-        self.bfs(tree)
+        return self.bfs(root)
         
         
