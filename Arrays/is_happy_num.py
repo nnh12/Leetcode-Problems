5327940d4ab039f09880d621b12c2b0abf0165b0
nathan hsiao <nathanhsiao123@gmail.com>
2025-02-14 15:09:37 -0600

    use fast and slow pointer to check if num is happy

---
+++ Arrays/is_happy_num.py
@@ -0,0 +1,36 @@
+class Solution(object):
+
+    def split_num(self, n):
+        nums = []
+        while (n >= 1):
+            new_num = n % 10
+            n /= 10
+            nums.append(new_num)
+        
+        new_num = 0
+        for n in nums:
+            new_num += n ** 2
+        
+        return new_num
+        
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        slow = n
+        fast = n
+        count = 0
+
+        while (count == 0 or (fast != slow)):
+            if fast == 1:
+                return True            
+            count = 1
+            fast = self.split_num(self.split_num(fast))
+            slow = self.split_num(slow)
+        
+        if fast == 1:
+            return True
+        else:
+            return False
+        
