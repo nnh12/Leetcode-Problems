eb446b58e2c3fabc9863a911937ed94b75914ebe
nathan hsiao <nathanhsiao123@gmail.com>
2025-02-14 15:32:28 -0600

    making sure I'm still sane with two sum

---
+++ Arrays/two_sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+
+        for i in range(len(nums)):
+            if ((target - nums[i]) in dict):
+                return [dict[target - nums[i]], i]
+            else:
+                dict[nums[i]] = i
+           
+        
+        return [0]
