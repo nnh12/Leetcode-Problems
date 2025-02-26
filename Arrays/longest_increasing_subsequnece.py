db6f4066ff224c299aa7425c42a87e66eb5757f7
nathan hsiao <nathanhsiao123@gmail.com>
2025-02-26 16:10:49 -0600

    longest increasing subsequnce

---
+++ Arrays/longest_increasing_subsequnece.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [1] * len(nums)
+        
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+        
+        print(dp)
+        return max(dp)
