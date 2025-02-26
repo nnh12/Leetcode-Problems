d52afe22af0137ee587bb902f960ec2e0b6b8bb9
nathan hsiao <nathanhsiao123@gmail.com>
2025-02-26 15:50:48 -0600

    implementation that passes all test cases

--- Trees/longest_word_dictionary.py
+++ Trees/longest_word_dictionary.py
@@ -1,13 +1,26 @@
 class Node(object):
     def __init__(self):
-        self.children = {}
+        self.childern = {}
         self.end = False
 
 class Solution(object):
 
     def __init__(self):
         self.root = Node()
-
+    
+    def add_node(self, cur_node, char):
+        cur_node.childern[char] = Node()
+        cur_node = cur_node.childern[char]
+        return cur_node
+    
+    def comp(self, cur_string, string):
+        if len(cur_string) == len(string):
+            if cur_string < string:
+                string = cur_string
+        elif len(cur_string) > len(string):
+            string = cur_string
+        
+        return string
 
     def longestWord(self, words):
         """
@@ -17,19 +30,31 @@ class Solution(object):
         words = sorted(words)
         print(words)
 
-        if len(words[0]) != 1:
-            return ""
-
         count = 0
-        cur_node = self.root
-        str = ""
-        for word in words:
-            for c in range(count + 1, len(word) -   ):
-                if word[c] not in cur_node.children:
-                    cur_node.children[c] = Node()
-                    cur_node = cur_node.children[c]
-                    str += word[c]
+        string = ""
+
+        for word in words:             
+            cur_node = self.root
+            cur_string = ""
+
+            if len(word) == 1:
+                string = self.comp(word[0], string)
+                cur_node = self.add_node(cur_node, word[0])
+
+            if len(word) > 1 and word[0] in cur_node.childern:
+
+                for index in range(len(word)):
+                    if index != len(word) - 1 and word[index] in cur_node.childern:
+                        cur_string += word[index]
+                        cur_node = cur_node.childern[word[index]]
+                    
+                    elif index == len(word) - 1:
+                        cur_node = self.add_node(cur_node, word[index])
+                        cur_string += word[index]
+
+                        print(cur_string, string)
+                        string = self.comp(cur_string, string)
+                    else:
+                        break
             
-            count += 1
-        
-        return str
+        return string
