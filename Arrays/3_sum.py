da9e13458e703a8bf6f59155ac5f2b3a5ff679c4
nathan hsiao <nathanhsiao123@gmail.com>
2025-03-03 14:55:11 -0600

    correct implementation passes all test cases

--- Arrays/3_sum.py
+++ Arrays/3_sum.py
@@ -8,26 +8,34 @@ class Solution(object):
         a = 0
         nums = sorted(nums)
 
-        while (a < len(nums) - 2 ):
+        while (nums[a] <= 0 and a < len(nums) - 2):
             left = a + 1
             right = len(nums) - 1
 
-            while left < right:
-                sum = nums[left] + nums[a] + nums[right]
-                print(a, left, right)
+            if a > 0 and nums[a] == nums[a - 1]:
+                a += 1
+                continue
+
+            while (left < right):
+                sum = nums[a] + nums[left] + nums[right]
 
                 if sum == 0:
-                    sublist = sorted([  nums[left] , nums[a], nums[right]  ])
-                    if sublist not in list:
-                        list.append(sublist)
+                    list.append([nums[a], nums[left], nums[right]])
                     left += 1
                     right -= 1
-                    
-                elif (sum < 0):
+                    while (left < right and nums[left] == nums[left - 1]):
+                        left += 1
+                    while (left < right and nums[right] == nums[right + 1]):
+                        right -= 1
+
+                elif sum < 0:
                     left += 1
-                elif (sum > 0):
+                elif sum > 0:
                     right -= 1
             
             a += 1
 
+
         return list
+
+    
