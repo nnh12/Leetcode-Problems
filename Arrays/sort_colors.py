754b088ee5f9527620b5dc5f789883fa121cf198
nathan hsiao <nathanhsiao123@gmail.com>
2025-03-07 13:55:34 -0600

    sort colors using two pointer technique

--- Arrays/sort_colors.py
+++ Arrays/sort_colors.py
@@ -4,28 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        zeros = 0
-        ones = 0
-        twos = 0
 
-        for i in nums:
-            if i == 0:
-                zeros += 1
-            elif i == 1:
-                ones += 1
-            elif i == 2:
-                twos += 1
-        
+        left = 0
+        right = len(nums) - 1
+        itr = 0
 
-        for i in range(len(nums)):
-            if zeros > 0:
-                nums[i] = 0
-                zeros -=1
-            elif ones > 0:
-                nums[i] = 1
-                ones -= 1
-            elif twos > 0:
-                nums[i] = 2
-                twos -= 1
-        
+        while (itr <= right):
+            if nums[itr] == 0:
+                temp = nums[itr]
+                nums[itr] = nums[left]
+                nums[left] = temp
+
+                left += 1
+                itr += 1
+
+            elif nums[itr] == 2:
+                temp = nums[itr]
+                nums[itr] = nums[right]
+                nums[right] = temp
+
+                right -= 1
+            elif nums[itr] == 1:
+                itr += 1        
         
