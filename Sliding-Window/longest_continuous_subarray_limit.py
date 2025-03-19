67752424eac9daaa240d7bbf52ecd4703412e456
nathan hsiao <nathanhsiao123@gmail.com>
2025-03-19 16:02:55 -0500

    correct implementation using two queues
    - use two queues to keep track of maximum and minimum for each window
    - keep iterate low until the limit is met

--- Sliding-Window/longest_continuous_subarray_limit.py
+++ Sliding-Window/longest_continuous_subarray_limit.py
@@ -5,25 +5,29 @@ class Solution(object):
         :type limit: int
         :rtype: int
         """
-        max_heap = []
-        min = []
-        heapq.heapify(max_heap)
-        heapq.heapify(min)
-        low = 0
+        max_q = deque()
+        min_q = deque()
         length = 0
+        low = 0
 
-        for high in range(len(nums)):
-            heapq.heappush(max_heap, -1*nums[high])
-            heapq.heappush(min, nums[high])
+        for high in range(len(nums)):           
+            while (max_q and max_q[-1] < nums[high] ):
+                max_q.pop()
+            
+            while (min_q and min_q[-1] > nums[high]):
+                min_q.pop()
+            
+            max_q.append(nums[high])
+            min_q.append(nums[high])
 
-            if abs((-1 * max_heap[0]) - min[0]) > limit:
-                while (low < high and abs((-1 * max_heap[0]) - min[0]) > limit ):
-                    max_heap.remove(-1 * nums[low])
-                    min.remove(nums[low])
-                    heapq.heapify(max_heap)
-                    heapq.heapify(min)
+            if abs(max_q[0] - min_q[0]) > limit:
+                while (low < high and abs(max_q[0] - min_q[0]) > limit):
+                    if min_q[0] == nums[low]:
+                        min_q.popleft()
+                    if max_q[0] == nums[low]:
+                        max_q.popleft()
                     low += 1
-                  
-            length = max(length, high - low + 1)
-                
+
+            length = max(length, high - low + 1)   
+            
         return length
