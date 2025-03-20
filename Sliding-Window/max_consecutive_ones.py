8e499a1cd7040f6144286d1e53a5a52858dedcbe
nathan hsiao <nathanhsiao123@gmail.com>
2025-03-20 15:59:42 -0500

    simplified implementation
    Add correct sliding window to include count of zeros and ones in window
    Constraint must exit when low is equal to high

--- Sliding-Window/max_consecutive_ones.py
+++ Sliding-Window/max_consecutive_ones.py
@@ -11,27 +11,18 @@ class Solution(object):
         low = 0 
 
         for high in range(len(nums)): 
-            
             if nums[high] == 0:
                 zeros += 1
             else:
                 ones += 1
 
-            if zeros > k and k != 0:
-                print('here')
-                length = max(length, high - low)
-                while (low < high and zeros > k):
-                    if nums[low] == 0:
-                        zeros -= 1
-                    else:
-                        ones -= 1
-                    low += 1
-            else:
-                if k == 0 and nums[high] == 0 or (k == 0 and high == len(nums) - 1):
-                    print(high, ones)
-                    length = max(length, ones)
-                    ones = 0
-                elif k != 0:
-                    length = max(length, ones + zeros)        
+            while (low <= high and zeros > k):
+                if nums[low] == 0:
+                    zeros -= 1
+                else:
+                    ones -=1
+                low += 1
+
+            length = max(length, zeros + ones) 
 
         return length
