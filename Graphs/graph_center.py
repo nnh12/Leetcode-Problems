39a1688bf885c552c0e497cdb21cb8b03387271c
nathan hsiao <nathanhsiao123@gmail.com>
2025-03-25 17:15:23 -0500

    fix updates case by only checking first two edges since center is connected to all

--- Graphs/graph_center.py
+++ Graphs/graph_center.py
@@ -4,26 +4,7 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-        matrix = {}
-        n = 0
-
-        for edge in edges:
-            n = max(edge[0], edge[1], n)
-
-            if edge[0] not in matrix:
-                matrix[edge[0]] = []
-                matrix[edge[0]].append(edge[1])
-            else:
-                matrix[edge[0]].append(edge[1])
-
-            if edge[1] not in matrix:
-                matrix[edge[1]] = []
-                matrix[edge[1]].append(edge[0])
-            else:
-                matrix[edge[1]].append(edge[0])               
-
-        for node in matrix:
-            if len(matrix[node]) == n - 1:
-                return node
-        
-        return 0
+        i, j = edges[0][0], edges[0][1]
+        if i in edges[1]:
+            return i
+        return j
