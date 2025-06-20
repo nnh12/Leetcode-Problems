301599a968a654828576202cb094acc4890b5ccf
nathan hsiao <nathanhsiao123@gmail.com>
2025-06-20 11:50:05 -0500

    learning recursion through Pascal's triangle

--- Dynamic-Programming/Pascals_triangle.py
+++ Dynamic-Programming/Pascals_triangle.py
@@ -1,20 +1,33 @@
 class Solution(object):
-     
 
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
-        list = []
-        for row in range(numRows):
-            sub_list = []
-            for col in range(row + 1):
-                if col == 0 or col == row:
-                    sub_list.append(1)
-                else:
-                    cur = list[row-1][col-1] + list[row-1][col]
-                    sub_list.append(cur)
-            list.append(sub_list) 
+    def get_number(self, row, col, sub_list, list):
+        if col == row:
+            sub_list.append(1)
+            return sub_list
+        else:   
+            if col == 0:
+                cur = 1
+            else:
+                cur = list[row-1][col-1] + list[row-1][col]
+            sub_list.append(cur)
+            if self.get_number(row, col + 1, sub_list, list):
+                return sub_list
+
+    def helper(self, row, rowIndex, list):
+        if row == rowIndex:
+            return list
         
-        return list
+        sub_list = self.get_number(row, 0, [], list)
+        
+        list.append(sub_list)
+        if self.helper(row + 1, rowIndex, list):
+            return list
+
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        res = self.helper(0, rowIndex + 1, [])
+        return res[rowIndex]
+        
